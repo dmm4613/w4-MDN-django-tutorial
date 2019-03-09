57a705aebcca82bd3fbd2f28d50bd37fd2a9339b
Dan Miller <dmm4613@gmail.com>
2019-03-08 21:59:20 -0500

    worked through part 8 and some of 9

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+from datetime import date
+from django.contrib.auth.models import User
 import uuid # Required for unique book instances
 
 
@@ -55,6 +57,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -62,6 +65,12 @@ class BookInstance(models.Model):
         ('a', 'Available'),
         ('r', 'Reserved'),
     )
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     status = models.CharField(
         max_length=1,
@@ -73,6 +82,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object."""
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -23,19 +23,28 @@ urlpatterns = [
 # Use include() to add paths from the catalog application 
 from django.urls import include
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
 
 #Add URL maps to redirect the base URL to our application
-from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
